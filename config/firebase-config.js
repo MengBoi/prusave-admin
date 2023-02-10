194cd0e8944dd84630e857ebf519b3a4d6a5a67b
TED <earmeng04@gmail.com>
2023-02-10 13:12:41 +0700

    minming project firebase credential config

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+// import { getDatabase, getStorage, getAuth } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
@@ -10,15 +10,14 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAjBeAiokldfxrxsj1nD7rYhMehpMEQQpI",
-  authDomain: "pcla-microsite.firebaseapp.com",
-  databaseURL:
-    "https://pcla-microsite-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "pcla-microsite",
-  storageBucket: "pcla-microsite.appspot.com",
-  messagingSenderId: "328487990954",
-  appId: "1:328487990954:web:5f0d9e8d6a15dca2b642d3",
-  measurementId: "G-5DMYGWQSD0",
+  apiKey: "AIzaSyD4n5CwTNUbmqcLBT6AO_t6uZ0Xqp0Hlpk",
+  authDomain: "mingming-f3a40.firebaseapp.com",
+  databaseURL: "https://mingming-f3a40.firebaseio.com",
+  projectId: "mingming-f3a40",
+  storageBucket: "mingming-f3a40.appspot.com",
+  messagingSenderId: "507736559855",
+  appId: "1:507736559855:web:49d77e1df69d9f0eec0b1a",
+  measurementId: "G-MX16VGLH2M",
 };
 
 // Initialize Firebase
@@ -26,4 +25,3 @@ const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
 export const storage = getStorage(app);
 export const authentication = getAuth(app);
-// const analytics = getAnalytics(app);
